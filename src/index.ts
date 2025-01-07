395f6167462d3f1439eed8b616b497c03e8b4d18
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2025-01-07 13:12:57 -0300

    feat: Integrate Mongoose for MongoDB connection and update Express server to handle database connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from "express"
+import mongoose from "mongoose"
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World - novo")
-})
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    app.get("/", (req, res) => {
+      res.send("Hello World - novo")
+    })
 
-const port = 3001
+    const port = 3001
 
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`)
-})
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`)
+    })
+
+  })
+  .catch(() => console.log('erro ao conectar no mongodb '))
