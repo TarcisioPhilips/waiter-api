d5679f60de09ead71f118d4dd190784f91c5802f
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2025-01-07 11:23:58 -0300

    chore: Set up ESLint and EditorConfig, add Express server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-let texto: string
+import express from "express"
 
-texto = "123"
+const app = express()
 
-console.log(texto)
+app.get("/", (req, res) => {
+  res.send("Hello World - novo")
+})
+
+const port = 3001
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running on http://localhost:${port}`)
+})
